c7b0017aa8f973bd4ee91adaa5ecfac37932978a
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-02-04 00:16:53 +0900

    pk mapping(2)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,9 +16,23 @@ public class JpaMain {
         tx.begin();
         try {
 //            Member findMember = em.find(Member.class, 1L);
-            Member member = new Member();
-            member.setUsername("C");
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("A");
+            Member member2 = new Member();
+            member2.setUsername("B");
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("===========");
+            em.persist(member1);//1, DB:51
+            em.persist(member2);//MEM
+            em.persist(member3);//MEM
+            System.out.println("member1 = " + member1);
+            System.out.println("member2 = " + member2);
+            System.out.println("member3 = " + member3);
+            System.out.println("===========");
+
+
 
             tx.commit();
         } catch (Exception e) {
